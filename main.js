1f81217603bf0b363f1f464f764fafd3c30c6d86
abdollah raafat <abdollah.raafat.daif@gmail.com>
2021-11-26 18:19:40 +0200

    Fix imgs js problem

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ const singers = [
     description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
      sed do eiusmod tempor Lorem ipsum dolor sit amet,
      consectetur adipiscing elit, sed do eiusmod tempor.`,
-    image: '../images/adel.jpg',
+    image: './images/adel.jpg',
   },
 
   {
