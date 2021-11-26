a4fe99d481cc920c419322b6b965c5298e7f6004
abdollah raafat <abdollah.raafat.daif@gmail.com>
2021-11-26 15:10:19 +0200

    Add script for mobile-menu & singers dynamic

---
+++ about.js
@@ -0,0 +1,28 @@
+// Menu-btn
+
+const menuIcon = document.getElementById('menu-id');
+
+menuIcon.addEventListener('click', () => {
+  document.getElementById('pop-list').style.display= "block";
+});
+
+const closeIcon = document.getElementById('close-Icon');
+
+closeIcon.addEventListener('click', () => {
+  document.getElementById('pop-list').style.display= "none";
+});
+
+const openPortfolio = document.getElementById('pop-buttons-1');
+openPortfolio.addEventListener('click', () => {
+  document.getElementById('pop-list').style.display= "none";
+});
+
+const openAbout = document.getElementById('pop-buttons-2');
+openAbout.addEventListener('click', () => {
+  document.getElementById('pop-list').style.display= "none";
+});
+
+const openContact = document.getElementById('pop-buttons-3');
+openContact.addEventListener('click', () => {
+  document.getElementById('pop-list').style.display= "none";
+});
